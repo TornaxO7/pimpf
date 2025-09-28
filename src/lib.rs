bdd72a1c6af58ac3361ebb53e27e26c085435ce5
TornaxO7 <tornax@proton.me>
2025-09-28 23:07:13 +0200

    pushing changes

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,31 @@
+mod parser;
+
 use std::path::Path;
 
+use crate::parser::Program;
+
 #[derive(thiserror::Error, Debug, Clone)]
-pub enum Error {}
+pub enum Error {
+    #[error(transparent)]
+    Parser(#[from] parser::Error),
+}
 
 #[derive(Debug, Clone)]
-pub struct Compiler {}
+pub struct Compiler {
+    source_code: String,
+    ast: Option<Program>,
+}
 
 impl Compiler {
     pub fn new<S: AsRef<str>>(source_code: S) -> Self {
-        todo!()
+        Self {
+            source_code: source_code.as_ref().to_string(),
+            ast: None,
+        }
     }
 
     pub fn compile(&mut self) -> Result<&mut Self, Error> {
+        let ast = parser::parse(&self.source_code)?;
         Ok(self)
     }
 
