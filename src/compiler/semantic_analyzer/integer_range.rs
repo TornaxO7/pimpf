c44fdf1d1daf25820728c41bac2fa6368abe4cbd
TornaxO7 <tornax@proton.me>
2026-04-27 19:35:51 +0200

    fix(intconst check): fix integer check

--- src/compiler/semantic_analyzer/integer_range.rs
+++ src/compiler/semantic_analyzer/integer_range.rs
@@ -2,9 +2,34 @@ use super::Error;
 use crate::compiler::parser::*;
 
 pub fn check(ast: &Program) -> Result<(), Error> {
-    let is_valid = |exp: &Exp| match exp {
+    for statement in &ast.0 {
+        match statement {
+            Statement::Decl(decl) => {
+                if let Some(exp) = &decl.exp {
+                    check_exp(exp)?;
+                }
+            }
+            Statement::Simp(simp) => check_exp(&simp.exp)?,
+            Statement::Return(exp) => check_exp(exp)?,
+        }
+    }
+    Ok(())
+}
+
+fn check_exp(exp: &Exp) -> Result<(), Error> {
+    match exp {
+        Exp::Ident(_) => Ok(()),
+        Exp::Add(a, b)
+        | Exp::Subtract(a, b)
+        | Exp::Multiply(a, b)
+        | Exp::Divide(a, b)
+        | Exp::Mod(a, b) => {
+            check_exp(a)?;
+            check_exp(b)
+        }
+        Exp::Neg(a) => check_exp(a),
         Exp::Decnum(decnum) => {
-            if decnum.parse::<u32>().is_ok() {
+            if decnum.parse::<i32>().is_ok() {
                 Ok(())
             } else {
                 Err(Error::InvalidIntconst(decnum.clone()))
@@ -17,19 +42,5 @@ pub fn check(ast: &Program) -> Result<(), Error> {
                 Err(Error::InvalidIntconst(format!("0x{}", hexnum.clone())))
             }
         }
-        _ => Ok(()),
-    };
-
-    for statement in &ast.0 {
-        match statement {
-            Statement::Decl(decl) => {
-                if let Some(exp) = &decl.exp {
-                    is_valid(exp)?;
-                }
-            }
-            Statement::Simp(simp) => is_valid(&simp.exp)?,
-            Statement::Return(exp) => is_valid(exp)?,
-        }
     }
-    Ok(())
 }
